1bf1efe7cce1b2f6dd330efb601e0ceb14d644dc
James Swineson <github@public.swineson.me>
2020-03-22 12:31:14 +0800

    impl SMPP PDU decoder

--- SMSGateway/SMPPListener/__init__.py
+++ SMSGateway/SMPPListener/__init__.py
@@ -1,3 +1,4 @@
+import binascii
 import logging
 import typing
 from dataclasses import dataclass
@@ -8,10 +9,26 @@ import smpplib.client
 import smpplib.consts
 import smpplib.gsm
 
+from .gsm_7bit_encoder import gsm_decode
 from ..IListener import IListener
 
 logger = logging.getLogger(__name__)
 smpp_devices: typing.List["SMPPDevice"] = []
+decoder_map = {
+    smpplib.consts.SMPP_ENCODING_DEFAULT: gsm_decode,
+    smpplib.consts.SMPP_ENCODING_IA5: "ascii",
+    smpplib.consts.SMPP_ENCODING_BINARY: "utf_16_be",
+    smpplib.consts.SMPP_ENCODING_ISO88591: "latin_1",
+    smpplib.consts.SMPP_ENCODING_BINARY2: "utf_16_be",
+    smpplib.consts.SMPP_ENCODING_JIS: "shift_jis",
+    smpplib.consts.SMPP_ENCODING_ISO88595: "iso8859_5",
+    smpplib.consts.SMPP_ENCODING_ISO88598: "iso8859_8",
+    smpplib.consts.SMPP_ENCODING_ISO10646: "utf_16_be",
+    smpplib.consts.SMPP_ENCODING_PICTOGRAM: "",
+    smpplib.consts.SMPP_ENCODING_ISO2022JP: "iso2022_jp",
+    smpplib.consts.SMPP_ENCODING_EXTJIS: "shift_jisx0213",
+    smpplib.consts.SMPP_ENCODING_KSC5601: "euc_kr",
+}
 
 
 @dataclass(init=False)
@@ -21,17 +38,41 @@ class SMPPDevice:
     smpp_client_listen_thread: Thread
 
 
-def smpp_message_send_handler(pdu):
+def smpp_message_send_handler(pdu: smpplib.command):
     logger.info(f"sent {pdu.sequence} {pdu.message_id}")
 
 
-def smpp_message_receive_handler(pdu):
+def smpp_message_receive_handler(pdu: smpplib.command):
     logger.info(f"delivered {pdu.receipted_message_id}")
     if pdu.command == "deliver_sm":
         # got new message
+
+        sender = ""
+        try:
+            sender = pdu.source_addr.decode('ascii')
+        except Exception as ex:
+            logger.exception(ex)
+            sender = str(pdu.source_addr)
+
+        # try to detect data encoding
+        decoder = decoder_map[smpplib.consts.SMPP_ENCODING_DEFAULT]
+        if pdu.data_coding in decoder_map:
+            decoder = decoder_map[pdu.data_coding]
+        else:
+            logger.warning(f"Unknown encoding {pdu.data_coding}")
+
+        encoded_message = ""
+        try:
+            if callable(decoder):
+                encoded_message = decoder(pdu.short_message)
+            else:
+                encoded_message = pdu.short_message.decode(decoder)
+        except Exception as ex:
+            logger.exception(ex)
+            encoded_message = binascii.hexlify(pdu.short_message).decode("ascii")
+
         logger.info(
-            f"New SMS from {pdu.source_addr.decode('ascii', errors='ignore')}: {pdu.short_message.decode('utf-16-be', errors='ignore')}")
-    # import ipdb; ipdb.set_trace()
+            f"New SMS from {sender}: {encoded_message}")
 
 
 class SMPPListener(IListener):
---
+++ SMSGateway/SMPPListener/gsm_7bit_encoder.py
@@ -0,0 +1,42 @@
+# https://stackoverflow.com/a/13131694/2646069
+# https://stackoverflow.com/a/2453027/2646069
+
+import binascii
+
+gsm = ("@£$¥èéùìòÇ\nØø\rÅåΔ_ΦΓΛΩΠΨΣΘΞ\x1bÆæßÉ !\"#¤%&'()*+,-./0123456789:;<=>?"
+       "¡ABCDEFGHIJKLMNOPQRSTUVWXYZÄÖÑÜ§¿abcdefghijklmnopqrstuvwxyzäöñüà")
+ext = ("````````````````````^```````````````````{}`````\\````````````[~]`"
+       "|````````````````````````````````````€``````````````````````````")
+
+
+def gsm_encode(plaintext):
+    res = ""
+    for c in plaintext:
+        idx = gsm.find(c);
+        if idx != -1:
+            res += chr(idx)
+            continue
+        idx = ext.find(c)
+        if idx != -1:
+            res += chr(27) + chr(idx)
+    return binascii.b2a_hex(res.encode('utf-8'))
+
+
+def gsm_decode(hexstr):
+    # res = binascii.a2b_hex(hexstr)
+    res = iter(hexstr)
+    result = []
+    for c in res:
+        if c == 27:
+            c = next(res)
+            result.append(ext[c])
+        else:
+            result.append(gsm[c])
+    return ''.join(result)
+
+
+if __name__ == "__main__":
+    # b'48656c6c6f20576f726c64'
+    print(gsm_encode("Hello World"))
+    # "Bush hid the facts"
+    print(gsm_decode(binascii.unhexlify(b'427573682068696420746865206661637473')))
